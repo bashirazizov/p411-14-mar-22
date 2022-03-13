1041f1c30b43eeda42eff73c45595fb9cdf4bf7c
bashirazizov <bashir.azizov@code.edu.az>
2022-03-14 03:14:05 +0400

    init

---
+++ vesper/vesper/Areas/Admin/Controllers/PartnersController.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using vesper.DAL;
+using vesper.Models;
+using vesper.Utils;
+
+namespace vesper.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class PartnersController : Controller
+    {
+        private readonly VesperDbContext db;
+        private readonly IWebHostEnvironment env;
+        public PartnersController(VesperDbContext _db, IWebHostEnvironment _env)
+        {
+            db = _db;
+            env = _env;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await db.Partners.ToListAsync());
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Add(Partner partner)
+        {
+            if (!ModelState.IsValid) return View();
+            Partner duplicate = await db.Partners.FirstOrDefaultAsync(x => x.Name == partner.Name);
+            if (duplicate != null)
+            {
+                ModelState.AddModelError("Name", "Name unique olmalidir.");
+                return View();
+            }
+
+            if (!partner.Image.IsImage())
+            {
+                ModelState.AddModelError("Image", "Fayl sekil olmalidir.");
+                return View();
+            }
+
+            if (!partner.Image.IsValidSize(500))
+            {
+                ModelState.AddModelError("Image", "Fayl maksimum 500kb ola biler.");
+                return View();
+            }
+
+            partner.Img = await partner.Image.Upload(env.WebRootPath, @"assets\img\clients");
+
+            await db.Partners.AddAsync(partner);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index", "Partners");
+        }
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) return RedirectToAction("Index", "Partners");
+            Partner partner = await db.Partners.FindAsync(id);
+            if (partner == null) return RedirectToAction("Index", "Partners");
+            return View(partner);
+        }
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return RedirectToAction("Index", "Partners");
+            Partner partner = await db.Partners.FindAsync(id);
+            if (partner == null) return RedirectToAction("Index", "Partners");
+            return View(partner);
+        }
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            if (id == null) return RedirectToAction("Index", "Partners");
+            Partner partnerToDelete = await db.Partners.FindAsync(id);
+            if (partnerToDelete == null) return RedirectToAction("Index", "Partners");
+
+            string filePath = Path.Combine(env.WebRootPath, @"assets\img\clients", partnerToDelete.Img);
+            if (System.IO.File.Exists(filePath))
+            {
+                System.IO.File.Delete(filePath);
+            }
+
+            db.Partners.Remove(partnerToDelete);
+            await db.SaveChangesAsync();
+
+            return RedirectToAction("Index", "Partners");
+        }
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return RedirectToAction("Index", "Partners");
+            Partner partner = await db.Partners.FindAsync(id);
+            if (partner == null) return RedirectToAction("Index", "Partners");
+            return View(partner);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Partner partner) {
+
+
+            if (!ModelState.IsValid) return View();
+
+            if (partner.Image != null)
+            {
+                if (!partner.Image.IsImage())
+                {
+                    ModelState.AddModelError("Image", "Fayl sekil olmalidir.");
+                    return View(partner);
+                }
+
+                if (!partner.Image.IsValidSize(500))
+                {
+                    ModelState.AddModelError("Image", "Fayl maksimum 500kb ola biler.");
+                    return View(partner);
+                }
+
+                string filePath = Path.Combine(env.WebRootPath, @"assets\img\clients", partner.Img);
+                if (System.IO.File.Exists(filePath))
+                {
+                    System.IO.File.Delete(filePath);
+                }
+
+                partner.Img = await partner.Image.Upload(env.WebRootPath, @"assets\img\clients");
+            }
+
+            db.Partners.Update(partner);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index", "Partners");
+        }
+    }
+}
---
+++ vesper/vesper/Areas/Admin/Controllers/ProjectsController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using vesper.DAL;
+using vesper.Models;
+using vesper.Utils;
+
+namespace vesper.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class ProjectsController : Controller
+    {
+        private readonly VesperDbContext db;
+        private readonly IWebHostEnvironment env;
+
+        public ProjectsController(VesperDbContext _db, IWebHostEnvironment _env)
+        {
+            db = _db;
+            env = _env;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await db.Projects.ToListAsync());
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Add(Project project)
+        {
+            if (!ModelState.IsValid) return View();
+            Project duplicate = await db.Projects.FirstOrDefaultAsync(x => x.Name == project.Name);
+            if (duplicate != null)
+            {
+                ModelState.AddModelError("Name", "Name unique olmalidir.");
+                return View();
+            }
+
+            if (!project.ImageFile.IsImage())
+            {
+                ModelState.AddModelError("ImageFile", "Fayl sekil olmalidir.");
+                return View();
+            }
+
+            if (!project.ImageFile.IsValidSize(500))
+            {
+                ModelState.AddModelError("ImageFile", "Fayl maksimum 500kb ola biler.");
+                return View();
+            }
+
+            project.Image = await project.ImageFile.Upload(env.WebRootPath, @"assets\img\portfolio");
+
+            await db.Projects.AddAsync(project);
+            await db.SaveChangesAsync();
+
+            if (project.ImageList!=null && project.ImageList.Length>0)
+            {
+                foreach (IFormFile item in project.ImageList)
+                {
+                    if (!item.IsImage())
+                    {
+                        ModelState.AddModelError("ImageList", item.FileName + "fayli sekil deyil.");
+                        db.Projects.Remove(db.Projects.Find(project.Id));
+                        await db.SaveChangesAsync();
+                        return View();
+                    }
+
+                    if (!item.IsValidSize(500))
+                    {
+                        ModelState.AddModelError("ImageList", item.FileName + "faylinin olcusu boyukdur.");
+                        db.Projects.Remove(db.Projects.Find(project.Id));
+                        await db.SaveChangesAsync();
+                        return View();
+                    }
+
+                    ProjectImage pi = new ProjectImage();
+                    pi.Img = await item.Upload(env.WebRootPath, @"assets\img\portfolio");
+                    pi.ProjectId = project.Id;
+
+                    await db.ProjectImages.AddAsync(pi);
+                }
+                await db.SaveChangesAsync();
+            }
+            return RedirectToAction("Index", "Projects");
+        }
+    }
+}
